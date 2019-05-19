93ce0f117df1f9a6c88cb095c055243eabb0fbb8
Batuhan Çam <batuc06@gmail.com>
2019-05-19 21:35:21 +0300

    service layer added

--- src/main/java/com/employee/restservices/employeerestservices/rest/EmployeeRestController.java
+++ src/main/java/com/employee/restservices/employeerestservices/rest/EmployeeRestController.java
@@ -2,8 +2,10 @@ package com.employee.restservices.employeerestservices.rest;
 
 import com.employee.restservices.employeerestservices.dao.EmployeeDAO;
 import com.employee.restservices.employeerestservices.entity.Employee;
+import com.employee.restservices.employeerestservices.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,18 +16,20 @@ import java.util.List;
 public class EmployeeRestController {
 
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @Autowired
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO){
-        employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
 
     }
 
     @GetMapping("/employees")
     public List<Employee> GetEmployees(){
 
-        return employeeDAO.findAll();
+        return employeeService.findAll();
 
     }
+
+
 }
---
+++ src/main/java/com/employee/restservices/employeerestservices/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.employee.restservices.employeerestservices.service;
+
+import com.employee.restservices.employeerestservices.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+    public Employee findById(int id);
+    public void save(Employee employee);
+    public void deleteById(int id);
+
+}
