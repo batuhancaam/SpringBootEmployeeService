45a299df5223047062feb046deb61da2f49f222e
Batuhan Çam <batuc06@gmail.com>
2019-05-19 20:24:49 +0300

    inital commit and project setup

---
+++ src/main/java/com/employee/restservices/employeerestservices/EmployeeRestservicesApplication.java
@@ -0,0 +1,13 @@
+package com.employee.restservices.employeerestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestservicesApplication.class, args);
+	}
+
+}
