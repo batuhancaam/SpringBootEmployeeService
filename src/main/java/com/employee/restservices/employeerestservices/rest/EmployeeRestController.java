cacf8ae4a9469bda64ac9fa39e64f3c92adca0aa
Batuhan Çam <batuc06@gmail.com>
2019-05-19 21:56:54 +0300

    Endpoints added

--- src/main/java/com/employee/restservices/employeerestservices/rest/EmployeeRestController.java
+++ src/main/java/com/employee/restservices/employeerestservices/rest/EmployeeRestController.java
@@ -4,10 +4,7 @@ import com.employee.restservices.employeerestservices.dao.EmployeeDAO;
 import com.employee.restservices.employeerestservices.entity.Employee;
 import com.employee.restservices.employeerestservices.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,39 @@ public class EmployeeRestController {
 
         return employeeService.findAll();
 
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee GetEmployee(@PathVariable int id){
+        Employee employee = employeeService.findById(id);
+
+        return employee;
+
+    }
+
+    @PostMapping("/employess")
+    public Employee addEmployee(@RequestBody Employee employee){
+
+        employee.setId(0);
+        employeeService.save(employee);
+        return employee;
+
+    }
+
+    @PutMapping("/employess")
+    public Employee updateEmployee(@RequestBody Employee employee){
+
+        employeeService.save(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public String deleteEmployee(@PathVariable int id){
+
+        employeeService.deleteById(id);
+
+        return "Employee Deleted id = "+id;
     }
 
 
